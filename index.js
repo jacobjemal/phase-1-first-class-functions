d0123a809fefb32b0f307c193debc58f4fc2e020
jacob jemal <jacobjemal7@gmail.com>
2022-03-21 13:18:02 -0400

    commit tree 72aa5af8bd3c69ff0006c3ef204bd819a5430bfd

---
+++ index.js
@@ -0,0 +1,12 @@
+const receivesAFunction = (callback) => {
+    callback()
+}
+function returnsANamedFunction() {
+    return function namedFn() {
+      console.log("this is a named function");
+    };
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return () => console.log("this is an anonymous function");
+  }
